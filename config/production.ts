71e2dcf7091b5b30aefa2354e1ba9fcf30bbf34d
Gwen <creepthewolf@icloud.com>
2020-05-18 06:09:19 -0400

    das

--- config/production.ts
+++ config/production.ts
@@ -3,8 +3,8 @@ export default {
     mongoString: process.env.DATABASE, // Your MongoDB connection string
     defaultPrefix: '-', // The default prefix
     developers: ['279910519467671554'], // Discord User IDs of you and fellow developers. These can use commands that are devOnly
-    infoChannel: '709866011800633436', // Discord Channel ID of a channel, where info should be logged to
-    errorChannel: '709866100686585876', // Discord Channel ID where errors should be logged to
+    infoChannel: '711882829918896238', // Discord Channel ID of a channel, where info should be logged to
+    errorChannel: '711882844171010068', // Discord Channel ID where errors should be logged to
     accentColor: '#eb4034',
     backgroundImage: 'autumn-forest.jpg',
     imgurID: process.env.AUTUMN_IMGUR_ID
