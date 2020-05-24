1d08536d02545694863cb83945eb83247f639d02
Gwen <creepthewolf@icloud.com>
2020-05-24 03:58:39 -0400

    delete settings on leave

--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -2,11 +2,14 @@ import { Client } from '../interfaces/Client';
 import { updateActivity } from '../index';
 //import { getGuildSettings } from '../database';
 import { Guild, TextChannel, MessageEmbed } from 'discord.js';
+import { GuildSettings } from '../database/schemas/GuildSettings';
 
 export default async (client: Client, guild: Guild) => {
     // Get the info channel
     const infoChannel = client.channels.cache.get(client.config.infoChannel) || (await client.channels.fetch(client.config.infoChannel));
     if (!infoChannel || !(infoChannel instanceof TextChannel)) throw new Error('Provided info channel is unreachable or not a text channel.');
+
+    GuildSettings.deleteOne({ guild: guild.id });
 
     const embed = new MessageEmbed()
         .setAuthor('Bot Information', client.user?.displayAvatarURL({ dynamic: true, format: 'png' }))
