d2d30f45317de03292ec23686ca98677c8e0ca5b
Gwen <creepthewolf@icloud.com>
2020-05-31 15:49:24 -0400

    Shit

--- src/events/message.ts
+++ src/events/message.ts
@@ -22,10 +22,6 @@ export default async (client: Client, message: Message) => {
 
     const verification = guildSettings?.verification;
 
-    const kek = ['<:kekwHands:716365354917429359>', '<:sadkek:716365351436288011>', '<:KEKW:716365353852207174>', '<:kekjpg:716365352262696991>'];
-
-    if (message.content === 'kek') return message.channel.send(kek[Math.floor(Math.random() * 4)]);
-
     // Verification Module
     if (guildSettings && guildSettings.verification.enabled) {
         if (!message.guild || !message.member || !verification?.enabled || message.author.bot) return;
@@ -109,6 +105,25 @@ export default async (client: Client, message: Message) => {
             return;
         }
     }
+
+    const kek = [
+        '<:kekwHands:716365354917429359>',
+        '<:sadkek:716365351436288011>',
+        '<:KEKW:716365353852207174>',
+        '<:kekjpg:716365352262696991>',
+        '<:KEKWPog:716736949288697986>',
+        '<:KEKW1:716736951696359466><:KEKW2:716736957623042128>\n<:KEKW3:716736956427534397><:KEKW4:716736954313736253>',
+        '<:KEKWFAKE:716736955202928726>',
+        '<a:KEKWDEAD:716736960294551562>',
+        '<:KEKWPRIDE:716736950303850557>',
+        '<a:KEKWPLS:716736963591536723>',
+        '<:KekWat:716736952371511559>',
+        '<a:3KEKW:716736961339064390>'
+    ];
+
+    if (message.content.toLowerCase().includes('kek') && (!prefix || !message.content.startsWith(prefix)))
+        return message.channel.send(kek[Math.floor(Math.random() * kek.length)]);
+
     if (!prefix || !message.content.startsWith(prefix)) return;
     if (!message.content.replace(prefix, '').length && pingRegex.test(prefix))
         return client.sendEmbed(message, 'Prefix', undefined, `My prefix is set to \`${guildPrefix}\`\n\nDo \`${guildPrefix}help\` for a list of commands`);
