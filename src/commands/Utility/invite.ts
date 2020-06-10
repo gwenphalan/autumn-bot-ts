07e28c3cb8d68ccec67dda3aa370da742615b562
Gwen <creepthewolf@icloud.com>
2020-06-10 16:56:13 -0400

    Shit

---
+++ src/commands/Utility/invite.ts
@@ -0,0 +1,21 @@
+import { Command, AMessage } from '../../interfaces/Client';
+import { PromptManager } from '../../interfaces/helpers/PromptManager';
+
+const callback = async (_message: AMessage, _args: {}, prompt: PromptManager) => {
+    return prompt.embed('Please give me a new home!', `• Invite: http://autm.fr/autumnbot\n• Website: https://www.autumnbot.net/`);
+};
+
+export const command: Command = {
+    name: 'invite',
+    category: 'Utility',
+    module: 'invite',
+    aliases: [],
+    description: 'Provides the invite link for the bot',
+    args: [],
+    devOnly: false,
+    guildOnly: false,
+    NSFW: false,
+    userPermissions: [],
+    botPermissions: [],
+    callback: callback
+};
