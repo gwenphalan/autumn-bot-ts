cf7ea43daa54ee26e7cf712bae90496efc0698b0
Gwen <creepthewolf@icloud.com>
2020-06-12 17:24:34 -0400

    Smug and Baka commands

---
+++ src/commands/Fun/smug.ts
@@ -0,0 +1,31 @@
+import { Command, AMessage } from '../../interfaces/Client';
+import { neko } from '../../neko';
+import Color from 'color';
+import { MessageEmbed } from 'discord.js';
+import { PromptManager } from '../../interfaces/helpers/PromptManager';
+
+const callback = async (message: AMessage, _args: {}, _prompt: PromptManager) => {
+    const hue = Math.floor(Math.random() * 360);
+    const pastel = 'hsl(' + hue + ', 100%, 80%)';
+
+    const color = Color(pastel).hex();
+
+    const result = await neko.sfw.smug();
+
+    message.channel.send(new MessageEmbed().setColor(color).setImage(result.url).setTimestamp());
+};
+
+export const command: Command = {
+    name: 'smug',
+    category: 'Fun',
+    module: 'Fun',
+    aliases: [],
+    description: 'Is someone being a baka??',
+    args: [],
+    devOnly: false,
+    guildOnly: false,
+    NSFW: false,
+    userPermissions: [],
+    botPermissions: [],
+    callback: callback
+};
