70065835648dd55d976801438faecb379fee861d
GwenBebe <creepthewolf@icloud.com>
2020-08-15 23:36:18 -0500

    Setting Up Travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,8 +3,3 @@ echo "***********[ Removing IP from logs ]***********"
 sshpass -p "$SERVER_PASS" ssh "$SERVER_USERNAME"@"$SERVER_NAME" "echo Hidden" > /dev/null 2>&1
 echo "***********[ Updating Container ]***********"
 sshpass -p "$SERVER_PASS" ssh "$SERVER_USERNAME"@"$SERVER_NAME" "cd ~/$WORK_DIR && git pull && sudo docker build -t autumnbot . && sudo docker-compose stop && sudo docker-compose up -d"
-echo "***********[ Cleanup ]***********"
-CLEANUP="sudo docker system prune -f"
-IMAGE_CLEANUP="sudo docker image prune --all -f"
-sshpass -p "$SERVER_PASS" ssh "$SERVER_USERNAME"@"$SERVER_NAME" "$CLEANUP"
-sshpass -p "$SERVER_PASS" ssh "$SERVER_USERNAME"@"$SERVER_NAME" "$IMAGE_CLEANUP"
