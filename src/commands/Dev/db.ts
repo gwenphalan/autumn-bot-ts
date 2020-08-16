475ec6dc945655c91bff853543e5ba82ba1b7957
GwenBebe <creepthewolf@icloud.com>
2020-08-15 21:48:12 -0500

    Updated Database Dev Command

--- src/commands/Dev/db.ts
+++ src/commands/Dev/db.ts
@@ -1,18 +1,43 @@
 import { Command, AMessage } from '../../interfaces/Client';
-import { getGuildSettings } from '../../database';
 import { client } from '../..';
 import { PromptManager } from '../../interfaces/helpers/PromptManager';
+import { GuildSettings } from '../../database/schemas/GuildSettings';
 
-const callback = async (message: AMessage, _args: {}, _prompt: PromptManager) => {
-    let i = 0;
+const callback = async (_message: AMessage, _args: {}, prompt: PromptManager) => {
+    let c = 0;
+    let d = 0;
     await Promise.all(
-        client.guilds.cache.map(g => {
-            getGuildSettings(g.id);
-            i++;
+        client.guilds.cache.map(async g => {
+            const guildSettings = await GuildSettings.findOne({ guild: g.id });
+
+            if (!guildSettings) {
+                await GuildSettings.create({
+                    guild: g.id,
+                    general: {},
+                    moderation: { enabled: false },
+                    verification: { enabled: false },
+                    welcome: { enabled: false }
+                });
+
+                c++;
+            }
         })
     );
 
-    message.reply(`Updated ${i} Database Entries`);
+    const guilds = await GuildSettings.find();
+
+    for (let i = 0; i < guilds.length; i++) {
+        const guild = guilds[i];
+
+        const guildSearch = client.guilds.cache.get(guild.guild);
+
+        if (!guildSearch) {
+            d++;
+            guild.deleteOne();
+        }
+    }
+
+    return prompt.sendMsg('Updated Database', `Created ${c} Entries\nDeleted ${d} Entries`);
 };
 
 export const command: Command = {
